b4e75760372f4c59a0933e70fc090ae2276837bc
jiangyue95 <yuejiang811@gmail.com>
2026-07-11 13:21:16 +0100

    feat: add S3 file storage foundation
    Introduce AWS SDK v2 and S3-backed file storage abstraction:
    - FileStorage interface with upload and presigned-URL generation
    - S3StorageOperator implementation (S3Client + S3Presigner)
    - S3Config beans and S3Properties bound to aws.s3.*
    - AWS SDK BOM in dependencyManagement
    
    Not yet wired to any endpoint; avatar feature builds on this.

---
+++ src/main/java/com/yue/jobcomparer/storage/FileStorage.java
@@ -0,0 +1,8 @@
+package com.yue.jobcomparer.storage;
+
+public interface FileStorage {
+
+    String upload(byte[] content, String originalFilename, String contentType);
+
+    String generatePresignedUrl(String key);
+}
